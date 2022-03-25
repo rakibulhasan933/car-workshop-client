b769dfca9588ac74bfd4af628ed332c596d54b7f
rakibulhasan933 <rakibulmd933@gmail.com>
2022-03-25 23:13:13 +0600

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Walker</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarText">
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Services</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Dashboard</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <button className='btn btn-outline-success'>Login</button>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
