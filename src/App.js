f6b9257e09132fa8fcf87e24df926d725ea40fdb
rakibulhasan933 <rakibulmd933@gmail.com>
2022-03-26 20:07:42 +0600

    some new feature added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Features from './components/Features/Features';
 import Header from './components/Header/Header';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <>
       <Header />
+      <Features />
+      <Services />
     </>
   );
 }
